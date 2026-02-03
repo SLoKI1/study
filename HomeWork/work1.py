2cbd7dfa0bdcab0a5929c618ce56cd566df2436a
SLoKI1 <frumasg@gmail.com>
2026-02-03 20:18:49 +0900

    done: home Work, edit def

---
+++ HomeWork/work1.py
@@ -0,0 +1,10 @@
+
+
+
+
+
+def avg(*num):
+    return sum(num)/len(num)
+
+
+print(avg(1,2,6,8))
--- les1.py
+++ les1.py
@@ -1,20 +1,47 @@
-# import random
-# a = []
-# for i in range(10):
-#     a.append(random.randint(1,125))
+import random
 
+a = []
+for i in range(10):
+    a.append(random.randint(1,125))
 
-a = [123, 26, 59, 96, 17, 118, 26, 28, 59, 42]
+# a = [103, 26, 59, 96, 17, 118, 26, 28, 59, 42]
 
 def detect_nechotnoe(arr):
+    newArr = []
     a = 0 
     b = 0
     for i in arr:
         if i%2!=0:
             b += i
             a += 1
-    minarr = min(arr)
-    maxarr = max(arr)
-    return f"Нечетных: {a}\nСумма: {b}\nМинимальное: {minarr}\nМаксимальное: {maxarr}"
+            newArr.append(i)
+    minarr = min(newArr)
+    maxarr = max(newArr)
+    avg = sum(newArr)/len(newArr)
+    
+    return f"""
+{arr=}
+{newArr=}
+-----------------
+Нечетных: {a}
+Сумма: {b}
+Минимальное: {minarr}
+Максимальное: {maxarr}
+Среднее: {round(avg,2)}
+-----------------
+Индекс максимального: Индекс: {detect_index_num(newArr)[0]} - число: {detect_index_num(newArr)[1]}"""
+
+
+def detect_index_num(arr):
+    indexStorage = 0 
+    numStorage = 0
+    for index, num in enumerate(arr):
+        if num>numStorage:
+            indexStorage = index
+            numStorage = num
+
+    return indexStorage, numStorage
+
+
 
 print(detect_nechotnoe(a))
