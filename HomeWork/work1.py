0e7b9895e162d4836edd6a2c8f7330f55d3d01f9
SLoKI1 <frumasg@gmail.com>
2026-02-07 23:35:58 +0900

    add: metr, kg, copeyca conventor

--- HomeWork/work1.py
+++ HomeWork/work1.py
@@ -1,10 +1,25 @@
 
+# def avg(*num):
+#     return sum(num)/len(num)
+
+# print(avg(1,2,6,8))
 
 
 
+# def metrConv(sm):
+#     metr = sm//100
+#     otherSm = sm%100
+#     return f"{metr} метра, {otherSm} см"
+# print(metrConv(1233))
 
-def avg(*num):
-    return sum(num)/len(num)
+# def kgConv(g):
+#     kg = g//1000
+#     otherG = g%1000
+#     return f"{kg} кг, {otherG} грамм"
+# print(kgConv(1233))
 
-
-print(avg(1,2,6,8))
+# def copeycaConv(copeyca):
+#     rub = copeyca//100
+#     otherCopeyca = copeyca%100
+#     return f"{rub} рублей, {otherCopeyca} копеек"
+# print(copeycaConv(1233))
