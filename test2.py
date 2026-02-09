141157016b2056fe0e88a2e071003ab9e56bc784
SLoKI1 <frumasg@gmail.com>
2026-02-09 14:41:40 +0900

    qwe

--- test2.py
+++ test2.py
@@ -1,12 +1,14 @@
 def asd(*countPrice):
+    if len(countPrice)%2!=0:
+        return "Ошибка"
 
-# int(len(countPrice)/2)
     bank = 0
-    for i in range(1, 4):
-        # bank += countPrice[i+1] * countPrice[i+2]
-        print(countPrice[i])
-        print(countPrice[i])
+    for i in range(0, len(countPrice), 2):
+        bank += countPrice[i] * countPrice[i+1]
     return bank
 
-asd(2,2,6,4,5,5)
-# print()
+print(asd(2,2,6,4,5,5))
+# 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
+
+# git config --global user.email "frumasg@gmail.com"
+# git config --global user.name "SLoKI1"
