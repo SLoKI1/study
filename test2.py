259b3580523384c6b288ba577bb9af986b437156
SLoKI1 <frumasg@gmail.com>
2026-02-09 14:41:33 +0900

    add: test2.1

---
+++ test2.py
@@ -0,0 +1,12 @@
+def asd(*countPrice):
+
+# int(len(countPrice)/2)
+    bank = 0
+    for i in range(1, 4):
+        # bank += countPrice[i+1] * countPrice[i+2]
+        print(countPrice[i])
+        print(countPrice[i])
+    return bank
+
+asd(2,2,6,4,5,5)
+# print()
