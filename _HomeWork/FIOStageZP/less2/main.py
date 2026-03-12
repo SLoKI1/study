66986af81fc9ed71e5e6c6701538f6199b08c346
SLoKI1 <frumasg@gmail.com>
2026-03-12 11:18:25 +0900

    done: less2

--- _HomeWork/FIOStageZP/less2/main.py
+++ _HomeWork/FIOStageZP/less2/main.py
@@ -1,6 +1,29 @@
-
-
-
 def iniciali(inputFile, outputFile):
-    while open("data.txt", "r", encoding="utf-8") a contentInput:
+    with open(inputFile, "r", encoding="utf-8") as contentInput:
+        all = ""
         for content in contentInput:
+            line = content.split()
+            all += f"{line[0]} {line[1][0]}. {line[2][0]}. \n"
+        with open(outputFile, "w", encoding="utf-8") as contentOutput:
+            contentOutput.write(all)
+
+def mony(inputFile, outputFile):
+    with open(inputFile, "r", encoding="utf-8") as contentInput:
+        counterrMember = 0
+        sumStage = 0
+        bank = 0
+        for content in contentInput:
+            line = content.split()
+            counterrMember += 1
+            sumStage += int(line[3])
+            zp = float(line[4])
+            ndfl = zp * 0.1
+            resZp = round(zp - ndfl,2)
+            bank += resZp
+        with open(outputFile, "w", encoding="utf-8") as contentOutput:
+            contentOutput.write(f"Cредний стаж: {round(sumStage/counterrMember,2)}\n")
+            contentOutput.write(f"Банк ЗП: {bank}")
+
+
+iniciali("data.txt","spisok.txt")
+mony("data.txt","finrez.txt")
